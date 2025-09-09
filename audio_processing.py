81441f3db4f8d4d37077f80b9a72a24e1be3fb4f
Abhishek Kumar <abhikrnigam@gmail.com>
2025-09-09 22:21:44 +0530

    initial commit, audio samples created but mic not working

---
+++ audio_processing.py
@@ -0,0 +1,42 @@
+import pyaudio
+import wave
+import logging 
+
+logging.basicConfig(level=logging.DEBUG,filename='audio_processing.log')
+logger = logging.getLogger()
+
+
+def recordAudio():
+    audio = pyaudio.PyAudio()
+    logger.info("Recording audio...")
+    stream = audio.open(format = pyaudio.paInt16,
+                        channels = 1,
+                        rate = 44100,
+                        input = True,
+                        frames_per_buffer = 1024)
+    frames = []
+    try:
+        while True:
+            data = stream.read(1024)
+            frames.append(data)
+    except KeyboardInterrupt:
+        logger.info("Stopping audio recording! ")
+        pass
+
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+    
+    sound_file = wave.open("output.wav","wb")
+    sound_file.setnchannels(1)
+    sound_file.setsampwidth(pyaudio.get_sample_size(pyaudio.paInt16))
+    sound_file.setframerate(44100)
+    sound_file.writeframes(b''.join(frames))
+    sound_file.close()
+
+logger.info("Audio recording successful! ")
+
+
+if __name__ == "__main__":
+    logger.info("Starting the process...")
+    recordAudio()
